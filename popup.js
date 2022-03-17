a709aba1ffeaa658c556318c9c04872f1fbf7621
LvKvA <l.koetsveld22@gmail.com>
2022-03-17 17:08:07 +0100

    Add button functionality

--- popup.js
+++ popup.js
@@ -1,1 +1,14 @@
+showConsumption.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: setPageBackgroundColor,
+    });
+  });
 
+function setPageBackgroundColor() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white"
+    document.body.style.fontFamily = "Arial"
+}
