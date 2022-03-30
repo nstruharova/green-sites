b5527bdccb7875e15627b4be8160dcc73cacfb7a
LvKvA <l.koetsveld22@gmail.com>
2022-03-30 12:13:18 +0200

    Add font alert

--- popup.js
+++ popup.js
@@ -72,16 +72,16 @@ Autoplay.addEventListener("click", async () => {
   console.log("Done: Check for autoplay video's")
 });
 
-Autoplay.addEventListener("click", async () => {
+Fonts.addEventListener("click", async () => {
   console.log("Start: Check for external fonts")
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: che,
+    function: checkFonts,
   });
 
-  console.log("Done: Check for autoplay video's")
+  console.log("Done: Check for external fonts")
 });
 
 LazyLoading.addEventListener("click", async () => {
@@ -217,7 +217,6 @@ function checkAutoplay(){
 }
 
 function checkFonts() {
-  console.log("=== Text ===");
   const systemFonts = new Set([
     // Windows 10
   'Arial', 'Arial Black', 'Bahnschrift', 'Calibri', 'Cambria', 'Cambria Math', 'Candara', 'Comic Sans MS', 'Consolas', 'Constantia', 'Corbel', 'Courier New', 'Ebrima', 'Franklin Gothic Medium', 'Gabriola', 'Gadugi', 'Georgia', 'HoloLens MDL2 Assets', 'Impact', 'Ink Free', 'Javanese Text', 'Leelawadee UI', 'Lucida Console', 'Lucida Sans Unicode', 'Malgun Gothic', 'Marlett', 'Microsoft Himalaya', 'Microsoft JhengHei', 'Microsoft New Tai Lue', 'Microsoft PhagsPa', 'Microsoft Sans Serif', 'Microsoft Tai Le', 'Microsoft YaHei', 'Microsoft Yi Baiti', 'MingLiU-ExtB', 'Mongolian Baiti', 'MS Gothic', 'MV Boli', 'Myanmar Text', 'Nirmala UI', 'Palatino Linotype', 'Segoe MDL2 Assets', 'Segoe Print', 'Segoe Script', 'Segoe UI', 'Segoe UI Historic', 'Segoe UI Emoji', 'Segoe UI Symbol', 'SimSun', 'Sitka', 'Sylfaen', 'Symbol', 'Tahoma', 'Times New Roman', 'Trebuchet MS', 'Verdana', 'Webdings', 'Wingdings', 'Yu Gothic',
@@ -237,7 +236,19 @@ function checkFonts() {
     font = it.next();
   }
 
-  [... new Set(fontArr)].forEach(f => console.log(f + " font is not a system font. Consider using pre-installed system fonts for decreased energy consumption."));
+  fontArr = [... new Set(fontArr)];
+  let fontNr = fontArr.length;
+
+  let alertText = ""
+  // .forEach(f => console.log(f + " font is not a system font. Consider using pre-installed system fonts for decreased energy consumption."));
+
+  if (fontArr.length > 0) {
+    alertText = "Found " + fontNr + " external fonts. Consider using pre-installed system fonts for decreased energy consumption.";
+  }
+  else {
+    alertText = "Found 0 external fonts, which is good for energy consumption. "
+  }
+  window.alert(alertText);
 }
 
 function checkLazyLoading(){
