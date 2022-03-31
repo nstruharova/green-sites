a83bb55c95f42d0fd6b6c8b7d74c6f16494ce5f5
LvKvA <l.koetsveld22@gmail.com>
2022-03-31 18:52:40 +0200

    Add icon

--- popup.js
+++ popup.js
@@ -304,107 +304,107 @@ function resetShadows() {
   });
 }
 
-function getImages() {
-  console.log("=== Images ===");
-  var imagesCollection = document.getElementsByTagName('img');
-  var images = Array.from(imagesCollection);
-  images.forEach((x) => {
-    console.log(x);
-    var score = 0;
-    // Check dimensions
-    var clientwidth = x.clientWidth;
-    var clientheight = x.clientHeight;
-    var naturalwidth = x.naturalWidth;
-    var naturalheight = x.naturalHeight;
+// function getImages() {
+//   console.log("=== Images ===");
+//   var imagesCollection = document.getElementsByTagName('img');
+//   var images = Array.from(imagesCollection);
+//   images.forEach((x) => {
+//     console.log(x);
+//     var score = 0;
+//     // Check dimensions
+//     var clientwidth = x.clientWidth;
+//     var clientheight = x.clientHeight;
+//     var naturalwidth = x.naturalWidth;
+//     var naturalheight = x.naturalHeight;
 
-    if (clientwidth == 0 || clientheight == 0) {
-      console.log("This image isn't rendered. Check why this image isn't rendered. Possibly you have applied lazy loading without setting a width or height, which is bad practise.")
-      score++;
-    }
-    if (clientwidth > 400 || clientheight > 400) {
-      console.log("This image is " + clientwidth + "x" + clientheight + "px, we consider this a large image. Maybe this image could be smaller?");
-      score++;
-    }
-    if (naturalwidth > 0 && naturalheight > 0 && (clientwidth < clientheight || clientheight < naturalheight)) {
-      console.log("This image is saved as a " + naturalwidth + "x" + naturalheight + "px image, but rendered as a " + clientwidth + "x" + clientheight + "px image. Consider saving the file with smaller dimensions. ");
-      score++;
-    }
+//     if (clientwidth == 0 || clientheight == 0) {
+//       console.log("This image isn't rendered. Check why this image isn't rendered. Possibly you have applied lazy loading without setting a width or height, which is bad practise.")
+//       score++;
+//     }
+//     if (clientwidth > 400 || clientheight > 400) {
+//       console.log("This image is " + clientwidth + "x" + clientheight + "px, we consider this a large image. Maybe this image could be smaller?");
+//       score++;
+//     }
+//     if (naturalwidth > 0 && naturalheight > 0 && (clientwidth < clientheight || clientheight < naturalheight)) {
+//       console.log("This image is saved as a " + naturalwidth + "x" + naturalheight + "px image, but rendered as a " + clientwidth + "x" + clientheight + "px image. Consider saving the file with smaller dimensions. ");
+//       score++;
+//     }
 
-    // Check lazy loading
-    if (x.loading == 'eager') {
-      console.log("This image is always loaded, regardless of whether it is shown. Consider lazy loading.");
-      score++;
-    }
+//     // Check lazy loading
+//     if (x.loading == 'eager') {
+//       console.log("This image is always loaded, regardless of whether it is shown. Consider lazy loading.");
+//       score++;
+//     }
 
-    // Check file format
-    var src = x.src;
-    if (src.includes(".png") || src.includes(".svg")) {
-      console.log("PNG and SVG should only be used if the precision of the file is very importent. Perhaps a .jpg file would suffice here?");
-      score++;
-    }
-    if (src.includes(".jpg") || src.includes(".jpeg") || src.includes(".png") || src.includes(".svg") || src.includes(".gif")) {
-      console.log("Consider using the .avif file format.");
-      score++;
-    }
-    const popup = document.createElement('div');
-    const p = document.createElement('p');
-    p.textContent = "testpopup";
-    popup.appendChild(p);
+//     // Check file format
+//     var src = x.src;
+//     if (src.includes(".png") || src.includes(".svg")) {
+//       console.log("PNG and SVG should only be used if the precision of the file is very importent. Perhaps a .jpg file would suffice here?");
+//       score++;
+//     }
+//     if (src.includes(".jpg") || src.includes(".jpeg") || src.includes(".png") || src.includes(".svg") || src.includes(".gif")) {
+//       console.log("Consider using the .avif file format.");
+//       score++;
+//     }
+//     const popup = document.createElement('div');
+//     const p = document.createElement('p');
+//     p.textContent = "testpopup";
+//     popup.appendChild(p);
 
-    switch (score) {
-      case 0:
-        console.log("green")
-        x.style.filter = "opacity(0.7) drop-shadow(0.5em 0.5em 5 #5beb34)"
-        break;
-      case 1:
-        console.log("quite green")
-        x.style.filter = "opacity(0.7) drop-shadow(0.5em 0.5em 0 #daf77c)"
-        break;
-      case 2:
-        console.log("yellow")
-        x.style.filter = "opacity(0.7) drop-shadow(0.5em 0.5em 0 #f0db3c)"
-        break;
-      case 3:
-        console.log("dirty yellow")
-        x.style.filter = "opacity(0.7) drop-shadow(0.5em 0.5em 0 #f0b73c)"
-        break;
-      case 4:
-        console.log("orange")
+//     switch (score) {
+//       case 0:
+//         console.log("green")
+//         x.style.filter = "opacity(0.7) drop-shadow(0.5em 0.5em 5 #5beb34)"
+//         break;
+//       case 1:
+//         console.log("quite green")
+//         x.style.filter = "opacity(0.7) drop-shadow(0.5em 0.5em 0 #daf77c)"
+//         break;
+//       case 2:
+//         console.log("yellow")
+//         x.style.filter = "opacity(0.7) drop-shadow(0.5em 0.5em 0 #f0db3c)"
+//         break;
+//       case 3:
+//         console.log("dirty yellow")
+//         x.style.filter = "opacity(0.7) drop-shadow(0.5em 0.5em 0 #f0b73c)"
+//         break;
+//       case 4:
+//         console.log("orange")
 
-        x.style.filter = "opacity(0.7) drop-shadow(0.5em 0.5em 0 #f2913d)"
-        x.appendChild(popup)
-        break;
-      case 5:
-        console.log("red")
-        x.style.filter = "opacity(0.7) drop-shadow(0.5em 0.5em 0 #ed6039)"
-        break;
-      case 6:
-        console.log("dead read");
-        x.style.filter = "opacity(0.7) drop-shadow(0.5em 0.5em 0 #ed3939)"
-        break;
-      default:
-    }
+//         x.style.filter = "opacity(0.7) drop-shadow(0.5em 0.5em 0 #f2913d)"
+//         x.appendChild(popup)
+//         break;
+//       case 5:
+//         console.log("red")
+//         x.style.filter = "opacity(0.7) drop-shadow(0.5em 0.5em 0 #ed6039)"
+//         break;
+//       case 6:
+//         console.log("dead read");
+//         x.style.filter = "opacity(0.7) drop-shadow(0.5em 0.5em 0 #ed3939)"
+//         break;
+//       default:
+//     }
 
 
-  });
-  console.log("There are " + images.length + " images");
-  return images;
-}
+//   });
+//   console.log("There are " + images.length + " images");
+//   return images;
+// }
 
-function getVideos() {
-  console.log("=== Videos ===");
-  var videoCollection = document.getElementsByTagName('video');
-  var videos = Array.from(videoCollection);
-  videos.forEach((x) => {
-    console.log(x)
-    if (x.autoplay == true) {
-      console.log("Consider switching of autoplay")
-    }
-    if (x.preload == "metadata") {
-      console.log("Consider not loading the metadata, you won't see a preview, but you download less data this way.")
-    } else if (x.preload != "none") {
-      console.log("Set the preload property to none to avoid unnecesary energy use.")
-    }
-  });
-  console.log("There are " + videos.length + " videos");
-}
+// function getVideos() {
+//   console.log("=== Videos ===");
+//   var videoCollection = document.getElementsByTagName('video');
+//   var videos = Array.from(videoCollection);
+//   videos.forEach((x) => {
+//     console.log(x)
+//     if (x.autoplay == true) {
+//       console.log("Consider switching of autoplay")
+//     }
+//     if (x.preload == "metadata") {
+//       console.log("Consider not loading the metadata, you won't see a preview, but you download less data this way.")
+//     } else if (x.preload != "none") {
+//       console.log("Set the preload property to none to avoid unnecesary energy use.")
+//     }
+//   });
+//   console.log("There are " + videos.length + " videos");
+// }
