6801b12432aa08f56873a522b6b247853d22714f
LvKvA <l.koetsveld22@gmail.com>
2022-03-31 18:14:20 +0200

    Fix font bug

--- popup.js
+++ popup.js
@@ -314,14 +314,14 @@ function getText() {
     'American Typewriter', 'Andale Mono', 'Arial', 'Arial Black', 'Arial Narrow', 'Arial Rounded MT Bold', 'Arial Unicode MS', 'Avenir', 'Avenir Next', 'Avenir Next Condensed', 'Baskerville', 'Big Caslon', 'Bodoni 72', 'Bodoni 72 Oldstyle', 'Bodoni 72 Smallcaps', 'Bradley Hand', 'Brush Script MT', 'Chalkboard', 'Chalkboard SE', 'Chalkduster', 'Charter', 'Cochin', 'Comic Sans MS', 'Copperplate', 'Courier', 'Courier New', 'Didot', 'DIN Alternate', 'DIN Condensed', 'Futura', 'Geneva', 'Georgia', 'Gill Sans', 'Helvetica', 'Helvetica Neue', 'Herculanum', 'Hoefler Text', 'Impact', 'Lucida Grande', 'Luminari', 'Marker Felt', 'Menlo', 'Microsoft Sans Serif', 'Monaco', 'Noteworthy', 'Optima', 'Palatino', 'Papyrus', 'Phosphate', 'Rockwell', 'Savoye LET', 'SignPainter', 'Skia', 'Snell Roundhand', 'Tahoma', 'Times', 'Times New Roman', 'Trattatello', 'Trebuchet MS', 'Verdana', 'Zapfino',
   ].sort());
 
-  const it = document.fonts.entries();
+  const it = document.fonts.values();
   let font = it.next();
   let fontArr = []
 
   // Check used fonts , if this is not a standard font display a message.
   while (!font.done) {
-    if (!(font.value[0].family in systemFonts)) {
-      fontArr.push(font.value[0].family);
+    if (!(font.family in systemFonts)) {
+      fontArr.push(font.family);
     }
     font = it.next();
   }
