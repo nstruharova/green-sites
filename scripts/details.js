fc60ea099d8365c299194f8703cebee0924caec7
LvKvA <l.koetsveld22@gmail.com>
2022-04-01 16:44:03 +0200

    Fix element deletion bug
    Removed details.js from manifest, which was causing the script to run on every page.

--- scripts/details.js
+++ scripts/details.js
@@ -5,7 +5,6 @@ var i;
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    console.log("yeet");
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
